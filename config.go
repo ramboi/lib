9477071d400fc92edf56fe84aa33d24c0425114c
shriram <132461179+ramboi@users.noreply.github.com>
2023-05-24 17:09:33 +0530

    Add files via upload

---
+++ config.go
@@ -0,0 +1,104 @@
+// config.go
+package lib
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc/peer"
+)
+
+type Config struct {
+	data map[string]interface{}
+	sync.RWMutex
+}
+
+const (
+	maxLogFileSize = 100
+)
+
+func (c *Config) Init() {
+	c.data = make(map[string]interface{})
+}
+
+func /*(c *Config)*/ OpenLogFile(logFileName string) (*os.File, error) {
+	file, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if fileInfo.Size() >= maxLogFileSize {
+		err := file.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		return rotateLogFile(logFileName)
+	}
+
+	return file, nil
+}
+
+func (c *Config) Put(ctx context.Context, value interface{}) {
+	if p, ok := peer.FromContext(ctx); ok {
+		c.Lock()
+		defer c.Unlock()
+		c.data[p.Addr.String()] = value
+		return
+	}
+	panic("unable to get peer from context.")
+}
+
+func (c *Config) Get(ctx context.Context) interface{} {
+	if p, ok := peer.FromContext(ctx); ok {
+		c.RLock()
+		defer c.RUnlock()
+		return c.data[p.Addr.String()]
+	}
+	panic("unable to get peer from context.")
+}
+
+func (c *Config) Delete(ctx context.Context) {
+	if p, ok := peer.FromContext(ctx); ok {
+		c.Lock()
+		defer c.Unlock()
+		delete(c.data, p.Addr.String())
+		return
+	}
+	panic("unable to get peer from context.")
+}
+
+func /*(c *Config)*/ rotateLogFile(logFileName string) (*os.File, error) {
+	dir, fileName := filepath.Split(logFileName)
+	ext := filepath.Ext(fileName)
+	baseName := fileName[:len(fileName)-len(ext)]
+	timestamp := time.Now().Format("20060102150405")
+
+	newFileName := filepath.Join(dir, baseName+"_"+timestamp+ext)
+
+	// Rename the current log file to the new file name
+	err := os.Rename(logFileName, newFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new log file
+	file, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// func Hello() {
+// 	println("heloo")
+// }
